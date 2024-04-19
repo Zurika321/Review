6b194741715ec90a22743e3c3ba0a3e76f60513b
Zurika321 <167527215+Zurika321@users.noreply.github.com>
2024-04-20 03:30:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+//Giữ khung ảnh đúng vị trí
 window.addEventListener("resize", function () {
   vitrikhunganh();
 });
+const content = document.getElementById("content");
+var khunganh = document.getElementById("Khung_anh");
 function vitrikhunganh() {
-  const content = document.getElementById("content");
-  var khunganh = document.getElementById("Khung_anh");
   var windowWidth = window.innerWidth;
   var widthkhunganh = khunganh.offsetWidth;
   var Topcontent = content.offsetTop;
@@ -11,7 +12,6 @@ function vitrikhunganh() {
   khunganh.style.top = Topcontent - Heightkhunganh / 2 + "px";
   khunganh.style.left = (windowWidth - widthkhunganh) / 2 + "px";
 }
-vitrikhunganh();
 
 // Giữ focus cho navbutton
 var allElements = document.querySelectorAll("*");
@@ -22,3 +22,54 @@ allElements.forEach(function (element) {
     }
   });
 });
+
+//chuyển trang
+var INFORMATION = document.getElementById("INFORMATION");
+var GAME = document.getElementById("GAME");
+var PROJECT = document.getElementById("PROJECT");
+var trang = [INFORMATION, GAME, PROJECT];
+function openTrang(num) {
+  for (let i = 0; i < trang.length; i++) {
+    if (i == num) {
+      trang[num].style.display = "block";
+      if (num != 0) {
+        khunganh.style.display = "none";
+      } else {
+        khunganh.style.display = "block";
+      }
+    } else {
+      trang[i].style.display = "none";
+    }
+  }
+  vitrikhunganh();
+}
+
+//copy noidung
+function copyText(noidung, sttbtn) {
+  // Ghi nội dung vào clipboard
+  navigator.clipboard
+    .writeText(noidung)
+    .then(function () {
+      document.getElementById(sttbtn).innerHTML = "Copied";
+      setTimeout(function () {
+        document.getElementById(sttbtn).innerHTML = "Copy";
+      }, 3000);
+    })
+    .catch(function (err) {
+      document.getElementById(sttbtn).innerHTML = "Error";
+      setTimeout(function () {
+        document.getElementById(sttbtn).innerHTML = "Copy";
+      }, 3000);
+    });
+}
+
+// click icon
+function icon() {
+  alert("mìk làm icon cho đẹp chứ ko muốn lộ thông tin quá nhiều :))");
+}
+// mới vô
+window.onload = function () {
+  document.getElementById("btnINFORMATION").click();
+  document.getElementById("btnINFORMATION").focus();
+};
+vitrikhunganh();
